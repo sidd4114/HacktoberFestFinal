69523520dedda33529a584d413757abe89e1ae14
GDG-RAIT <dsc.rait@gmail.com>
2024-10-06 00:14:18 +0530

    More Languages

---
+++ C/4.c
@@ -0,0 +1,27 @@
+// Remove Nth Node From End of List
+
+// Example 1:
+// Input: head = [1,2,3,4,5], n = 2
+// Output: [1,2,3,5]
+
+// Example 2:
+// Input: head = [1], n = 1
+// Output: []
+
+// Example 3:
+// Input: head = [1,2], n = 1
+// Output: [1]
+ 
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+struct ListNode* removeNthFromEnd(struct ListNode* head, int n) {
+    
+}
---
+++ Java/4.java
@@ -0,0 +1,31 @@
+// Remove Nth Node From End of List
+
+// Example 1:
+// Input: head = [1,2,3,4,5], n = 2
+// Output: [1,2,3,5]
+
+// Example 2:
+// Input: head = [1], n = 1
+// Output: []
+
+// Example 3:
+// Input: head = [1,2], n = 1
+// Output: [1]
+ 
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        
+    }
+}
