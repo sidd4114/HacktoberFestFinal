3d86a3624694d00db6da5140cf2c04eac04b3e3a
GDG-RAIT <dsc.rait@gmail.com>
2024-10-05 23:59:57 +0530

    C++ snippets

---
+++ C++/9.cpp
@@ -0,0 +1,26 @@
+// Given an integer array nums and two integers lower and upper, return the number of range sums that lie in [lower, upper] inclusive.
+
+// Range sum S(i, j) is defined as the sum of the elements in nums between indices i and j inclusive, where i <= j.
+
+ 
+
+// Example 1:
+
+// Input: nums = [-2,5,-1], lower = -2, upper = 2
+// Output: 3
+// Explanation: The three ranges are: [0,0], [2,2], and [0,2] and their respective sums are: -2, -1, 2.
+
+// Example 2:
+
+// Input: nums = [0], lower = 0, upper = 0
+// Output: 1
+
+class Solution {
+public:
+    int countRangeSum(vector<int>& nums, int lower, int upper) {
+        
+    }
+};
+ 
+
+
