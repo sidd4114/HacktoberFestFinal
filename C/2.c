8922d75b14d6491985ff716b1edae18d07ba8046
Manthan2006-tech <manthanc714@gmail.com>
2024-10-06 11:38:58 +0530

    404notfound
    404notfound

--- C/2.c
+++ C/2.c
@@ -28,6 +28,33 @@
 // 2 <= n <= 105
 // 0 <= height[i] <= 10^4
 
-int maxArea(int* height, int heightSize) {
-    
+
+#include<stdio.h>
+
+int maxWater(int arr[], int n)
+{
+ int res=0;
+
+ for (int i=1;i<n-1;i++)
+ {
+  int left=arr[i];
+  for(int j=0;j<i;j++)
+  if (arr[j]>left)
+    left=arr[j];
+  
+  int right=arr[i];
+  for(int j=i+1;j<n;j++)
+  if (arr[j]>right)
+    right=arr[j];
+
+  res+=(left < right ? left : right) - arr[i];  
+ }
+ return res;
+}
+int main()
+{   
+    int arr[]={1,8,6,2,5,4,8,3,7};
+    int n=sizeof(arr) / sizeof(arr[0]);
+    printf("%d", maxWater(arr, n));
+    return 0;
 }
